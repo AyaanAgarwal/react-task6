6203087f3316546baec728d4f914c32fd4ab8f87
Ayaan Agarwal <ayaan08vk18@gmail.com>
2023-10-14 18:40:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import "./App.scss";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import Home from "./container/home";
+import About from "./container/about";
+// import Resume from "./containers/resume";
+// import Skills from "./containers/skills";
+// import Portfolio from "./containers/portfolio";
+import Contact from "./container/contact";
+import Navbar from "./components/nav";
+import particles from "./utils.js/particles";
+
+function App() {
+  const location = useLocation();
+  console.log(location);
+
+  const handleInit = async (main) => {
+    await loadFull(main);
+  };
+
+  const renderParticleJsInHomePage = location.pathname === "/";
+
+  return (
+    <div className="App">
+      {/* particles js */}
+
+      {renderParticleJsInHomePage && (
+        <Particles id="particles" options={particles} init={handleInit} />
+      )}
+
+      {/* navbar */}
+      <Navbar />
+
+      {/* main page content */}
+      <div className="App__main-page-content">
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
